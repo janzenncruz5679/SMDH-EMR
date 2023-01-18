65380da0dc0707ad591d1e98e35c3957efc4c18b
ohmypotato <mekeni07@gmail.com>
2023-01-18 19:14:00 +0800

    Minor FE changes

--- resources/views/user/stationSection/labOptions.blade.php
+++ resources/views/user/stationSection/labOptions.blade.php
@@ -1,41 +1,44 @@
 @extends('layouts.main')
 
+{{-- test responsiveness using these colors sm:bg-red-200 md:bg-violet-300 lg:bg-blue-800 xl:bg-yellow-500 --}}
 @section('content')
-    <div {{-- test responsiveness using these colors sm:bg-red-200 md:bg-violet-300 lg:bg-blue-800 xl:bg-yellow-500 --}} class="grid fixed h-[94%] w-[86%] left-[275px] top-[59px] p-12">
+    <div class="grid fixed h-[94%] w-[86%] left-[275px] top-[59px] p-12">
         <div class="grid grid-rows-6 w-full">
             <div class="grid grid-cols-12 gap-14 ">
                 <a class="col-span-2 grid items-center p-6 text-zinc-900 hover:text-white bg-blue-100 hover:bg-blue-300 rounded-3xl shadow-md shadow-blue-200 hover:-translate-y-0.5 transform transition"
-                    href="{{ url('/patientPage/admission') }}">
+                    href="{{ url('billing') }}">
                     <div class="grid gap-3">
                         <div class="grid place-items-center">
-                            <i class="fa-solid fa-solid fa-bed text-[7rem]"></i>
+                            <i class="fa-solid fa-heart-pulse text-[7rem]"></i>
                         </div>
                         <div class="grid justify-center text-2xl font-[sans-serif]">
-                            <p class="grid justify-center">{{ __('Admission') }}</p>
-                        </div>
-                    </div>
-                </a>
-
-                <a class="col-span-2 grid items-center p-6 text-zinc-900 hover:text-white bg-blue-100 hover:bg-blue-300 rounded-3xl shadow-md shadow-blue-200 hover:-translate-y-0.5 transform transition"
-                    href="{{ url('patientPage') }}">
-                    <div class="grid gap-3">
-                        <div class="grid place-items-center">
-                            <i class="fa-solid fa-truck-medical text-[7rem]"></i>
-                        </div>
-                        <div class="grid justify-center text-2xl font-[sans-serif]">
-                            <p class="grid justify-center">{{ __('Emergency') }}</p>
+                            <p class="grid justify-center">{{ __('Vital Signs') }}</p>
                         </div>
                     </div>
                 </a>
 
                 <a class="col-span-2 grid items-start p-6 text-zinc-900 hover:text-white bg-blue-100 hover:bg-blue-300 rounded-3xl shadow-md shadow-blue-200 hover:-translate-y-0.5 transform transition"
-                    href="{{ url('patientPage') }}">
+                    href="{{ url('billing') }}">
                     <div class="grid gap-3">
                         <div class="grid place-items-center">
-                            <i class="fa-solid fa-kit-medical text-[7rem]"></i>
+                            <i class="fa-solid fa-notes-medical text-[7rem]"></i>
                         </div>
                         <div class="grid justify-center text-2xl font-[sans-serif]">
-                            <p class="grid justify-center">{{ __('Outpatients') }}</p>
+                            <p class="grid justify-center">{{ __('Discharge') }}</p>
+                            <p class="grid justify-center">{{ __('Summary') }}</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a class="col-span-2 grid items-start p-6 text-zinc-900 hover:text-white bg-blue-100 hover:bg-blue-300 rounded-3xl shadow-md shadow-blue-200 hover:-translate-y-0.5 transform transition"
+                    href="{{ url('labOptions') }}">
+                    <div class="grid gap-3">
+                        <div class="grid place-items-center">
+                            <i class="fa-solid fa-suitcase-medical text-[7rem]"></i>
+                        </div>
+                        <div class="grid justify-center text-2xl font-[sans-serif]">
+                            <p class="grid justify-center">{{ __('Medication &') }}</p>
+                            <p class="grid justify-center">{{ __('Treatment') }}</p>
                         </div>
                     </div>
                 </a>
--- resources/views/user/stations.blade.php
+++ resources/views/user/stations.blade.php
@@ -1,42 +1,47 @@
 @extends('layouts.main')
 
-
+{{-- test responsiveness using these colors sm:bg-red-200 md:bg-violet-300 lg:bg-blue-800 xl:bg-yellow-500 --}}
 @section('content')
-    <div
-        class="grid fixed h-[94%] w-[86%] left-[275px] top-[59px] p-12 sm:bg-red-200 md:bg-violet-300 lg:bg-blue-800 xl:bg-yellow-500">
-        <div class=" grid grid-cols-6 gap-16 h-[25%] w-full ">
-            <div class="grid w-full gap-[10px]">
-                <a class=" text-zinc-900 hover:text-white" href="{{ url('patientPage') }}">
-                    <div
-                        class=" bg-blue-100 hover:bg-blue-300 py-8 gap-4 rounded-3xl grid place-items-center shadow-md shadow-blue-200 hover:-translate-y-0.5 transform transition">
-                        <i class="fa-solid fa-x-ray text-[7rem]"></i>
-                        <div class="text-2xl font-[sans-serif]">
-                            {{ __('X-ray') }}
+    <div class="grid fixed h-[94%] w-[86%] left-[275px] top-[59px] p-12">
+        <div class="grid grid-rows-6 w-full">
+            <div class="grid grid-cols-12 gap-14 ">
+                <a class="col-span-2 grid items-center p-6 text-zinc-900 hover:text-white bg-blue-100 hover:bg-blue-300 rounded-3xl shadow-md shadow-blue-200 hover:-translate-y-0.5 transform transition"
+                    href="{{ url('patientPage') }}">
+                    <div class="grid gap-3">
+                        <div class="grid place-items-center">
+                            <i class="fa-solid fa-x-ray text-[7rem]"></i>
+                        </div>
+                        <div class="grid justify-center text-2xl font-[sans-serif]">
+                            <p class="grid justify-center">{{ __('X-ray') }}</p>
                         </div>
                     </div>
                 </a>
-            </div>
-            <div class="grid w-full gap-[10px]">
-                <a class=" text-zinc-900 hover:text-white" href="{{ url('patientPage') }}">
-                    <div
-                        class=" bg-blue-100 hover:bg-blue-300 py-8 gap-4 rounded-3xl grid place-items-center shadow-md shadow-blue-200 hover:-translate-y-0.5 transform transition">
-                        <i class="fa-solid fa-flask text-[7rem]"></i>
-                        <div class="text-2xl font-[sans-serif]">
-                            {{ __('Laboratory') }}
+
+
+                <a class="col-span-2 grid items-start p-6 text-zinc-900 hover:text-white bg-blue-100 hover:bg-blue-300 rounded-3xl shadow-md shadow-blue-200 hover:-translate-y-0.5 transform transition"
+                    href="{{ url('labOptions') }}">
+                    <div class="grid gap-3">
+                        <div class="grid place-items-center">
+                            <i class="fa-solid fa-flask text-[7rem]"></i>
+                        </div>
+                        <div class="grid justify-center text-2xl font-[sans-serif]">
+                            <p class="grid justify-center">{{ __('Laboratory') }}</p>
                         </div>
                     </div>
                 </a>
-            </div>
-            <div class="grid w-full gap-[10px]">
-                <a class=" text-zinc-900 hover:text-white" href="{{ url('billing') }}">
-                    <div
-                        class=" bg-blue-100 hover:bg-blue-300 py-8 gap-4 rounded-3xl grid place-items-center shadow-md shadow-blue-200 hover:-translate-y-0.5 transform transition">
-                        <i class="fa-solid fa-prescription-bottle-medical text-[7rem]"></i>
-                        <div class="text-2xl font-[sans-serif]">
-                            {{ __('Pharmacy') }}
+
+                <a class="col-span-2 grid items-start p-6 text-zinc-900 hover:text-white bg-blue-100 hover:bg-blue-300 rounded-3xl shadow-md shadow-blue-200 hover:-translate-y-0.5 transform transition"
+                    href="{{ url('billing') }}">
+                    <div class="grid gap-3">
+                        <div class="grid place-items-center">
+                            <i class="fa-solid fa-prescription-bottle-medical text-[7rem]"></i>
+                        </div>
+                        <div class="grid justify-center text-2xl font-[sans-serif]">
+                            <p class="grid justify-center">{{ __('Pharmacy') }}</p>
                         </div>
                     </div>
                 </a>
+
             </div>
         </div>
     </div>

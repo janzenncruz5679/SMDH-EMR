c3e3a90a04c32d35e90dda5df694c4b9a45d49e1
ohmypotato <mekeni07@gmail.com>
2023-02-05 09:54:08 +0800

    remove

--- resources/views/user/patientSection/admission.blade.php
+++ resources/views/user/patientSection/admission.blade.php
@@ -1,11 +1,1 @@
-@extends('layouts.main')
 
-@section('content')
-    <div class="absolute h-auto w-[86%] left-[275px] top-[59px] p-12 grid gap-8">
-        @livewire('update-multi-step-form')
-    </div>
-@endsection
-
-@push('custom_scripts')
-    @vite('resources/js/patientPage/liveSearch.js')
-@endpush

1ffdac22f28ce731543a522c556882efb51b3b08
ohmypotato <mekeni07@gmail.com>
2023-02-05 13:40:08 +0800

    try

--- resources/views/user/patientSection/addpatient.blade.php
+++ resources/views/user/patientSection/addpatient.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.main')
 @section('content')
-    <div class="addPatient absolute top-[59px] left-[275px] h-full w-[85.3vw] p-[45px] ">
-        @livewire('multi-step-form')
-    </div>
 @endsection
 
 @push('custom_scripts')

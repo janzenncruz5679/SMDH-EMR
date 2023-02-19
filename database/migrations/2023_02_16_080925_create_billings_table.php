9fa16abe4e35e42703346ac5faf9204b5d9abc4e
ohmypotato <mekeni07@gmail.com>
2023-02-19 23:05:51 +0800

    billing

--- database/migrations/2023_02_16_080925_create_billings_table.php
+++ database/migrations/2023_02_16_080925_create_billings_table.php
@@ -17,6 +17,14 @@ return new class extends Migration
             $table->id();
             $table->string('or_no')->nullable();
             $table->string('full_name')->nullable();
+            $table->decimal('total', 12, 2)->unsigned()->nullable();
+            $table->decimal('medicine', 12, 2)->unsigned()->nullable();
+            $table->decimal('lab', 12, 2)->unsigned()->nullable();
+            $table->decimal('xray', 12, 2)->unsigned()->nullable();
+            $table->decimal('ecg', 12, 2)->unsigned()->nullable();
+            $table->decimal('oxygen', 12, 2)->unsigned()->nullable();
+            $table->decimal('nbs', 12, 2)->unsigned()->nullable();
+            $table->decimal('income', 12, 2)->unsigned()->nullable();
             $table->timestamps();
         });
     }

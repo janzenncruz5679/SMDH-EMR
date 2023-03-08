c8b1ae7b44cc909687cdbd1f8167fd4f41fc1daa
rhonstratos <kizunaai20@gmail.com>
2023-03-09 03:27:14 +0800

    prepare patient.show page

--- app/Http/Controllers/PatientsController.php
+++ app/Http/Controllers/PatientsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Patients;
 use Illuminate\Http\Request;
 
 class PatientsController extends Controller
@@ -9,5 +10,11 @@ class PatientsController extends Controller
     public function index()
     {
         return view('pages.patients.index');
+    }
+
+    public function show(Patients $patient)
+    {
+        return view('pages.patients.show')
+            ->with(compact('patient'));
     }
 }

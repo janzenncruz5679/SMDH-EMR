53aa1a30399ae27eebe7cb2955462335bec14e1d
ohmypotato <mekeni07@gmail.com>
2023-04-07 07:57:19 +0800

    billing crud done

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Billing\UpdateBilling;
 use App\Models\Admission;
 use App\Models\Billing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BillingController extends Controller
 {
+    public function __construct(
+        private UpdateBilling $updateBilling,
+    ) {
+    }
 
     public function index()
     {
@@ -15,9 +21,8 @@ class BillingController extends Controller
         return view('user.billing.index', compact('billings'));
     }
 
-    public function create()
+    public function create(Billing $billing)
     {
-        //
     }
 
 
@@ -33,12 +38,22 @@ class BillingController extends Controller
 
     public function edit(Billing $billing)
     {
-        //
+        return view('user.billing.edit', compact('billing'));
     }
 
     public function update(Request $request, Billing $billing)
     {
-        //
+        try {
+            DB::beginTransaction();
+            $update = $this->updateBilling->handle($request, $billing);
+            // dd($update);
+            DB::commit();
+            return redirect()->route('billing.index');
+        } catch (\Exception $err) {
+            DB::rollBack();
+            dd($err);
+            return redirect()->back()->withErrors($err->getMessage());
+        }
     }
 
     public function destroy(Billing $billing)

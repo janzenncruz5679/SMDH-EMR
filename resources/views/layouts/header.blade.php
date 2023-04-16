206a72a1dc4ac8a2f44daad6a8051de62ce58006
ohmypotato <mekeni07@gmail.com>
2023-04-16 13:31:08 +0800

    for deployment change logo.png

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,7 +2,8 @@
     <div
         class="fixed min-w-full h-[7%] text-[2rem] bg-custom_hospital-400 left-0 top-0 flex space-between items-center font-[sans-serif] overflow-hidden">
         <div class="flex flex-row h-full gap-4 p-2">
-            <img src="{{ Vite::image('san_miguel_hospital_logo.png') }}" alt="logo">
+            {{-- <img src="{{ Vite::image('san_miguel_hospital_logo.png') }}" alt="logo"> --}}
+            <img src="../../assets/img/san_miguel_hospital_logo.png" alt="logo">
             <a class=" w-full text-white" href="{{ url('home') }}">
                 {{ __('SAN MIGUEL DISTRICT HOSPITAL') }}
             </a>

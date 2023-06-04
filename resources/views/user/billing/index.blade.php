2c6e1365aaab0bbbad1661784c0f5864ac06bbbe
ohmypotato <mekeni07@gmail.com>
2023-06-05 01:44:04 +0800

    responsive billing index

--- resources/views/user/billing/index.blade.php
+++ resources/views/user/billing/index.blade.php
@@ -1,34 +1,45 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="fixed h-[93%] w-[84%] left-[16%] top-[7%] flex flex-col gap-8 p-12">
-        <div class="h-20 bg-blue-300 flex items-center justify-center">
-            <label class="font-[sans-serif] font-semibold text-white tracking-wide text-4xl">
-                {{ __('Billing Summary') }}</label>
+    <div class="h-full w-full p-6 md:p-8 lg:p-12 flex flex-col gap-4 text-xl">
+        <div class="h-[10%] bg-blue-300 flex items-center justify-center">
+            <label class="font-[sans-serif] font-semibold text-white tracking-wide text-3xl md:text-4xl">
+                BIlling Summary</label>
         </div>
-        <div class="flex-grow w-full row-span-2 grid grid-cols-5 gap-8">
-            <div class="col-span-4 flex flex-col gap-8">
+        <div class="grid grid-cols-4 gap-4">
+            <div class="col-span-4 xl:col-span-3">
                 @if (isset($billings))
                     <table class="tracking-widest w-full">
                         <thead>
-                            <tr class="grid grid-cols-12 text-2xl">
-                                <th class="col-span-2 flex justify-center">OR #</th>
-                                <th class="col-span-4 flex justify-center">Name</th>
-                                <th class="col-span-2 flex justify-center">Bill Created</th>
-                                <th class="col-span-3 flex justify-center">Total Bill</th>
+                            <tr
+                                class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 2xl:grid-cols-12 text-xl xl:text-2xl">
+                                <th class="hidden 2xl:col-span-2 md:flex items-center justify-center text-center">OR #</th>
+                                <th
+                                    class="sm:col-span-2 md:col-span-2 lg:col-span-2 2xl:col-span-4 flex items-center justify-center text-center">
+                                    Name
+                                </th>
+                                <th class="hidden 2xl:col-span-2 lg:flex items-center justify-center text-center">Bill
+                                    Created</th>
+                                <th class="2xl:col-span-3 flex items-center justify-center text-center">Total
+                                    Bill</th>
                                 <th class="flex justify-center">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($billings as $billing)
-                                <tr class="grid grid-cols-12 even:bg-gray-200 odd:bg-white text-xl">
-                                    <td class="col-span-2 flex justify-center">{{ $billing->id }}</td>
-                                    <td class="col-span-4 flex justify-center">{{ $billing->full_name }}</td>
-                                    <td class="col-span-2 flex justify-center">
+                                <tr
+                                    class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 2xl:grid-cols-12 even:bg-gray-200 odd:bg-white xs:text-sm text-lg xl:text-xl">
+                                    <td class="hidden 2xl:col-span-2 md:flex items-center justify-center text-center">
+                                        {{ $billing->id }}</td>
+                                    <td
+                                        class="sm:col-span-2 md:col-span-2 lg:col-span-2 2xl:col-span-4 flex items-center justify-center text-center">
+                                        {{ $billing->full_name }}
+                                    </td>
+                                    <td class="hidden 2xl:col-span-2 lg:flex items-center justify-center text-center">
                                         {{ Carbon\Carbon::parse($billing->created_at)->format('F d, Y') }}</td>
-                                    <td class="col-span-3 flex justify-center">
+                                    <td class="2xl:col-span-3 flex items-center justify-center text-center">
                                         ₱{{ number_format($billing->grand_total, 2) }}</td>
-                                    <td class="flex justify-center">
+                                    <td class="flex items-center justify-center text-center">
                                         <div class="grid grid-cols-2 justify-center gap-4">
                                             <a href="{{ route('billing.show', $billing->id) }}"
                                                 class="editIcon hover:text-blue-300">
@@ -49,7 +60,7 @@
                     {{ $billings->links('pagination::custom_tailwind') }}
                 </div>
             </div>
-            <div class="h-full w-full bg-blue-300 p-8 shadow-lg shadow-blue-200 rounded-3xl">
+            <div class="hidden xl:block h-full w-full bg-blue-300 p-8 shadow-lg shadow-blue-200 rounded-3xl">
                 <div class="h-full flex flex-col gap-4">
                     <div class="">
                         <label class="font-[sans-serif] font-semibold text-white tracking-wide text-3xl">
--- resources/views/user/patientPage.blade.php
+++ resources/views/user/patientPage.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="h-full w-full xs:p-4 sm:p-8 xl:p-12 overflow-hidden">
-        <div class="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-5 xs:gap-4 sm:gap-8">
+        <div class="grid grid-cols-2 sm:grid-cols-3 2xl:grid-cols-5 xs:gap-4 sm:gap-8">
             <x-menu-card :url="route('admission.index')" text="Admission" fontAwesomeIcon="fa-solid fa-bed"></x-menu-card>
             <x-menu-card :url="route('emergency.index')" text="Emergency" fontAwesomeIcon="fa-solid fa-truck-medical">
             </x-menu-card>
